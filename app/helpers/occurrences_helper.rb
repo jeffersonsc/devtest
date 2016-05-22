5b753049668ceaeb1e54cef51b32ffdc9f9c00c6
Jefferson Cunha <jeffersondasilva14@gmail.com>
2016-05-22 14:11:10 -0300

    Add tweet link
    Signed-off-by: Jefferson Cunha <jeffersondasilva14@gmail.com>

--- app/helpers/occurrences_helper.rb
+++ app/helpers/occurrences_helper.rb
@@ -1,5 +1,13 @@
 module OccurrencesHelper
 	def twitter_link(username)
-		url = "https://twitter.com/" + username.gsub('@', '').strip
+		url = "https://twitter.com/" + username(username)
+	end
+
+	def tweet_link(username, tweet_id)
+		url = "https://twitter.com/#{username(username)}/status/#{tweet_id}"
+	end
+
+	def username(username)
+		username.gsub('@', '').strip
 	end
 end
--- spec/helpers/occurrences_helper_spec.rb
+++ spec/helpers/occurrences_helper_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe OccurrencesHelper, type: :helper do
-  describe "#convert date" do
-    it "returns true" do
+  describe "Return links for twitter" do
+    it "#returns true to profile link" do
       expect(helper.twitter_link("@jeffersoncunha")).to eq('https://twitter.com/jeffersoncunha')
     end
+
+    it "#Return link to tweet" do
+    	expect(helper.tweet_link("@jeffersoncunha", "12345678")).to eq('https://twitter.com/jeffersoncunha/status/12345678')
+  	end
+
   end
 end
